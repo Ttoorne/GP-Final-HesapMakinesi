08646aced4563f6b218a295b6dcaef7951499a53
Ttoorne <karakozhaev.bektur@gmail.com>
2023-06-12 04:37:48 +0600

    Hesplama

---
+++ CalcMvcWeb/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CalcMvcWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+      public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+            });
+
+
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson();
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Hesap Makinesi
+
+Bu proje, Windows Forms kullanarak basit bir hesap makinesi uygulamasını ve bu uygulamanın işlevselliğini doğrulamak için modül testlerini içerir.
+
+## Özellikler
+
+- Toplama, çıkarma, çarpma ve bölme gibi temel matematiksel işlemleri gerçekleştirme.
+- Pozitif, negatif ve ondalık sayılarla çalışabilme.
+- Kullanıcı dostu bir arayüz sağlama.
+- Modül testleri ile işlevsellik doğrulaması yapma.
+
+## Gereksinimler
+
+- Windows işletim sistemi.
+- .NET Framework 4.5 veya daha üstü.
+
+## Kurulum
+
+1. Bu projeyi bilgisayarınıza klonlayın veya indirin.
+
+   ```bash
+   git clone https://github.com/sizin-kullanici-adi/CalcApp30WithTests.git
+   ```
+
+2. Visual Studio gibi bir .NET geliştirme ortamında projeyi açın.
+
+3. Projeyi derleyin ve çalıştırın.
+
+## Kullanım
+
+- Uygulama başlatıldığında, basit bir hesap makinesi arayüzü görüntülenir.
+- Sayıları girmek için numara düğmelerini kullanın.
+- İşlem yapmak için operatör düğmelerini kullanın.
+- Sonucu görmek için "Sonuç" düğmesini tıklayın.
+- Modül testlerini çalıştırmak için test projesini derleyin ve çalıştırın.
